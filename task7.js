83dad9a4cb88e645511a8aeaf2b81b63ef97cffe
Arsenmvp <arsenklaptentko@gmail.com>
2025-06-19 09:31:41 +0300

    Update task7.js

--- task7.js
+++ task7.js
@@ -1,1 +1,7 @@
+function averageSalary(salaries) {
+  let values = Object.values(salaries);
+  if (values.length === 0) return 0;
+  let sum = values.reduce((acc, salary) => acc + salary, 0);
+  return sum / values.length;
+}
 
