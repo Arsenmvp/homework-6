14f7f5757953345146e11a53e29517f376bf88db
Arsenmvp <arsenklaptentko@gmail.com>
2025-06-19 09:27:36 +0300

    Update task5.js

--- task5.js
+++ task5.js
@@ -1,1 +1,15 @@
+let table = [
+  {
+    name: "Name",
+    values: ["Cris", "Alex", "Tom"]
+  },
+  {
+    name: "Age",
+    values: [25, 30, 28]
+  },
+  {
+    name: "Country",
+    values: ["USA", "Canada", "UK"]
+  }
+];
 
