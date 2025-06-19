3a57266c8c061e22dfdd33b1fb6678bce6575330
Arsenmvp <arsenklaptentko@gmail.com>
2025-06-19 09:23:40 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,1 +1,10 @@
+let car = {};
+car.model = "Toyota";
+car.speed = 120;
+car.run = function() {
+  console.log(`${car.model} їде зі швидкістю ${car.speed}`);
+};
+car.stop = function() {
+  console.log(`${car.model} зупинилася`);
+};
 
