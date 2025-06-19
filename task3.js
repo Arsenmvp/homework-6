bdc170bfc3c54fa674d942471132ef2e80f5a410
Arsenmvp <arsenklaptentko@gmail.com>
2025-06-19 09:25:35 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,1 +1,13 @@
+let tv = {
+  currentChannel: 1,
+  nextChannel() {
+    this.currentChannel++;
+  },
+  previousChannel() {
+    this.currentChannel--;
+  },
+  setChannel(number) {
+    this.currentChannel = number;
+  }
+};
 
